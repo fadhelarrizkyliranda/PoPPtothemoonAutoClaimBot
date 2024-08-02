76a6703742f99f5865c6ac3789648944b5ce344f
fadhelarrizkyliranda <fadhelarrizkyliranda@gmail.com>
2024-08-02 15:44:35 +0700

    Commit all files

--- README.md
+++ README.md
@@ -1,1 +1,33 @@
 # PoPPtothemoonAutoClaimBot
+## For Your Information
+This script interacts with the Moon Popp Club API to manage farming activities, log asset data, and track the current time. It uses `axios` for HTTP requests, `colors` for colored console output, and `dotenv` for managing environment variables.
+
+## Development Preparation
+### Clone Repo
+Clone from https://github.com/fadhelarrizkyliranda/PoPPtothemoonAutoClaimBot (choose `master` branch)
+
+### Setup Node, NPM, Yarn
+For windows and mac user you can [download node](https://nodejs.org/en/) and install.
+For linux user you can install with ppa the tutorial can be found [here](https://www.digitalocean.com/community/tutorials/how-to-install-node-js-on-ubuntu-18-04).
+
+### Package Installation
+Before start development and running the test you need to install packages
+- Open your favorite terminal.
+- and run `npm install` in your terminal
+- wait, and done
+
+### Environment Variables
+Create a .env file in the root directory and add the following:
+```js
+//Input your Bearer token
+AUTH_TOKEN=your_auth_token_here
+```
+# Run Test
+1. Open your terminal
+2. Extract it `node index.js`
+
+# Contact
+For any inquiries or feedback, please contact:
+Name: Fadhel Ar Rizky Liranda
+Email: fadhelarrizkyliranda@gmail.com
+GitHub: fadhelarrizkyliranda
---
+++ index.js
@@ -0,0 +1,124 @@
+// Importing necessary modules
+require('dotenv').config();
+const axios = require('axios');
+const colors = require('colors');
+const fs = require('fs');
+const { AUTH_TOKEN } = process.env;
+
+// Function to log to file
+function logToFile(message) {
+    const logMessage = `${message}\n`;
+    fs.appendFile('farming.log', logMessage, (err) => {
+      if (err) {
+        console.error('Error writing to log file:', err);
+      }
+    });
+}
+
+// Function to process the claim request
+async function fetchData() {
+  try {
+    // Configure request with endpoint and header
+    const response = await axios.get('https://moon.popp.club/moon/claim/farming', {
+      headers: {
+        'Authorization': AUTH_TOKEN
+      }
+    });
+    
+    // Log the response body
+    const message = response.data.msg;
+    console.log('Claim Farming:', message.green);
+  }catch (error) {
+    // Handle error if the request fails
+    console.error('Claim Farming:', error);
+    logToFile(`Claim Farming Error: ${error.message}`);
+  }
+}
+
+// Function to fetch farming data
+async function fetchFarmingData() {
+  try {
+    // Configure request with endpoint and header
+    const response = await axios.get('https://moon.popp.club/moon/farming', {
+      headers: {
+        'Authorization': AUTH_TOKEN
+      }
+    });
+    
+    // Log the response body
+    const message = response.data.msg;
+    console.log('Start Farming:', message.green);
+  } catch (error) {
+    // Handle error if the request fails
+    console.error('Start Farming:', error);
+    logToFile(`Start Farming Error: ${error.message}`);
+  }
+}
+
+// Function to fetch asset data
+async function fetchAssetData() {
+  try {
+    // Configure request with endpoint and header
+    const response = await axios.get('https://moon.popp.club/moon/asset', {
+      headers: {
+        'Authorization': AUTH_TOKEN
+      }
+    });
+    
+    // Log the response body
+    const message = response.data.data.sd;
+    console.log('Asset Amount (SD):', message);
+    logToFile(`Asset Amount (SD): ${message}`);
+  } catch (error) {
+    // Handle error if the request fails
+    console.error('Fetch Asset Data:', error);
+    logToFile(`Fetch Asset Data Error: ${error.message}`);
+  }
+}
+
+// Function to log the current time
+function logCurrentTime() {
+  const now = new Date();
+  const hours = now.getHours().toString().padStart(2, '0');
+  const minutes = now.getMinutes().toString().padStart(2, '0');
+  const seconds = now.getSeconds().toString().padStart(2, '0');
+  const currentTime = `Current Time: ${hours}:${minutes}:${seconds}`;
+  console.log(currentTime.yellow);
+  logToFile(currentTime);
+}
+
+// Function to fetch asset data every minute and check conditions
+async function fetchAssetPerMinute() {
+  try {
+    // Configure request with endpoint and header
+    const response = await axios.get('https://moon.popp.club/moon/asset', {
+      headers: {
+        'Authorization': AUTH_TOKEN
+      }
+    });
+    // Check if farming has ended
+    if (response.data.data.farmingEndTime === 0) {
+        console.log('===================================');
+        logToFile('===================================');
+        console.log('Farming has ended!'.red);
+        logCurrentTime();
+        await fetchData();
+        await fetchFarmingData();
+        await fetchAssetData();
+        console.log('=================================== \n');
+        logToFile('===================================');
+    }
+  } catch (error) {
+    // Handle error if the request fails
+    console.error('Fetch Asset Per Minute:', error);
+  }
+}
+
+// Function to run the fetchAssetPerMinute function continuously every 5 seconds
+function runContinuously() {
+  fetchAssetPerMinute();
+  setInterval(fetchAssetPerMinute, 15 * 1000); // Run every 5 seconds
+}
+
+// Start the continuous run
+runContinuously();
